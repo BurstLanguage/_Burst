641c487a27e35fab4b18ca2ae4335ebf6ec25bb9
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2016-07-03 20:18:19 -0500

    Added version information to project info Makefile
    Also added a target to dump project information
    
    Signed-off-by: Gigabyte-Giant <brynden.bielefeld@hotmail.com>

--- ProjectInfo.mk
+++ ProjectInfo.mk
@@ -2,7 +2,16 @@
 # BurstLanguage/ProjectInfo.mk
 # Defines general information about the project.
 #
-PROJECT_NAME := "Burst"
-PROJECT_OWNER := "Brynden 'Gigabyte Giant' Bielefeld"
-PROJECT_LICENSE := "MIT"
-PROJECT_REPOSITORY := "https://github.com/Gigabyte-Giant/Burst.git"
+NAME := "Burst"
+OWNER := "Brynden 'Gigabyte Giant' Bielefeld"
+LICENSE := "MIT"
+REPOSITORY := "https://github.com/Gigabyte-Giant/Burst.git"
+
+VERSION_MAJOR := "0"
+VERSION_MINOR := "0"
+VERSION_ITERATION := "0"
+VERSION_STRING := "v${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_ITERATION}"
+
+dump_project_info:
+	@echo "${NAME} ${VERSION_STRING}"
+	@echo "${LICENSE} license"
