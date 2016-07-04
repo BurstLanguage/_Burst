c7e8df6a5c0f560bf41ef1e4eec120d715605813
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2016-07-03 20:01:26 -0500

    Created Makefile that defines general info about project
    Signed-off-by: Gigabyte-Giant <brynden.bielefeld@hotmail.com>

---
+++ ProjectInfo.mk
@@ -0,0 +1,8 @@
+#
+# BurstLanguage/ProjectInfo.mk
+# Defines general information about the project.
+#
+PROJECT_NAME := "Burst"
+PROJECT_OWNER := "Brynden 'Gigabyte Giant' Bielefeld"
+PROJECT_LICENSE := "MIT"
+PROJECT_REPOSITORY := "https://github.com/Gigabyte-Giant/Burst.git"
