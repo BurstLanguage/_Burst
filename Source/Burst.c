45ad8d974c190149a21d69d9b84872f931b5e212
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2016-08-21 15:36:17 -0500

    Re-enable the parser, also make sure it's destroyed
    Signed-off-by: Gigabyte-Giant <brynden.bielefeld@hotmail.com>

--- Source/Burst.c
+++ Source/Burst.c
@@ -11,7 +11,7 @@ int main
 )
 {
     BurstLexer *pFileLexer = NULL;
-    // BurstParser *pFileParser = NULL;
+    BurstParser *pFileParser = NULL;
     
     if (BURST_MIN_ARG_COUNT > argumentCount)
     {
@@ -45,23 +45,24 @@ int main
         
         lexer_run(pFileLexer);
         
-        // if (BURST_SUCCESS != parser_create(pFileLexer->pAnalyzer->pOutputTokens,
-        //     &pFileParser))
-        // {
-        //     lexer_destroy(pFileLexer);
+        if (BURST_SUCCESS != parser_create(pFileLexer->pAnalyzer->pOutputTokens,
+            &pFileParser))
+        {
+            lexer_destroy(pFileLexer);
             
-        //     return BURST_FAIL;
-        // }
+            return BURST_FAIL;
+        }
         
-        // if (BURST_SUCCESS != parser_run(pFileParser))
-        // {
-        //     parser_destroy(pFileParser);
-        //     lexer_destroy(pFileLexer);
+        if (BURST_SUCCESS != parser_run(pFileParser))
+        {
+            parser_destroy(pFileParser);
+            lexer_destroy(pFileLexer);
             
-        //     return BURST_FAIL;
-        // }
+            return BURST_FAIL;
+        }
         
         lexer_destroy(pFileLexer);
+        parser_destroy(pFileParser);
     }
     
     return BURST_SUCCESS;
